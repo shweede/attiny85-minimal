476491cd1ae7cb2bc677910b153c3467eceb95f7
Mārcis <shweede@gmail.com>
2020-12-12 17:02:15 +0200

    feat: add startup indication Blink a lot at start, then blink normal.

--- blink.c
+++ blink.c
@@ -3,12 +3,21 @@
 
 int main(void)
 {
+    uint8_t pin = 3;
+    uint8_t pinMask = (1 << pin);
     DDRB |= 8;
+
+    for (int i = 0; i < 40; ++i)
+    {
+        PORTB ^= pinMask;
+        _delay_ms(100);
+    }
+
     while (1)
     {
-        _delay_ms(5000);
-        PORTB ^= 2;
+        _delay_ms(2000);
+        PORTB ^= pinMask;
         _delay_ms(100);
-        PORTB ^= 2;
+        PORTB ^= pinMask;
     }
 }
